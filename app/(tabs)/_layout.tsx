3953c73ebc7f8d96643223700e9c84d6bf31ed26
lima8002 <57651553+lima8002@users.noreply.github.com>
2025-07-13 14:42:36 +0100

    feat: Navigation setup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Redirect, Slot } from "expo-router";
+
+const _Layout = () => {
+  const isAuthenticated = true;
+
+  if (!isAuthenticated) return <Redirect href="/sign-in" />;
+
+  return <Slot />;
+};
+export default _Layout;
